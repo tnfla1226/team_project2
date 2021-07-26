2125f99051947ddd1dd1edba5e91c2020bd19954
tnfla1226 <tnfla1226@naver.com>
2021-07-27 00:43:24 +0900

    [21.07.27] team_project2 계산기 중간까지

--- app.js
+++ app.js
@@ -1,65 +1,38 @@
+let answer = '';
 
-let numArray = [];
-let val1 = '';
-let val2 = '';
+function operation (num) {
 
-
-
-
-function addNum(num) {
-    const $show = document.getElementById('show');
-
-    $show.value = $show.value + num;
-    $show.setAttribute('value', $show.value);
-    numArray.push(+$show.value);
-
-    val2 = numArray[numArray.length-1];
-    console.log(val2);
-    // console.log(numArray);
-  }
-  
-function calculate(operator) {
-    const $show = document.getElementById('show');
-    
-    if (operator === '+') {
-        $show.value = '';
-        // $numArray = [];
-        // val1 += val2;
-        // console.log(val1);
+    if (num !== '=') {
+        const $show = document.getElementById('show');
+        $show.value += num;
+        console.log($show.value);
     }
-}
 
-//   function calculate() {
-//     const $show = document.getElementById('show');
-//     const $answer = eval($show.value);
-//     const $displayAnswer = document.getElementById('answer');
-//     $displayAnswer.value = result;
-//   }
-  
+}
 
 (function () {
 
     const $buttons = document.querySelector('.buttons');
+    const $answer = document.querySelector('.answer');
     const $show = document.getElementById('show');
     
     // 숫자 버튼 클릭 이벤트 
     $buttons.addEventListener('click', e => {
-        if (e.target.matches('.num')) {
-            console.log('숫자버튼 클릭댐!');
-            addNum(e.target.textContent);
-            
+        if (e.target.matches('.check')) {
+            operation(e.target.value);
         }
-    });   
-
-    $buttons.addEventListener('click', e => {
-        if (e.target.matches('.plus') || e.target.matches('.minus') || e.target.matches('.divisor') || e.target.matches('.mul')) {
-            calculate(e.target.textContent);
-        }
-        
     });
 
-    // if (e.target.matches('.answer')) {
-    //     numArray = [];
-    // }
+    $answer.addEventListener('click', e => {
+        // answer = String(eval($show.value));
+       answer = (new Function ('return '+$show.value))();
 
+        $show.value = $show.value + '=' + String(answer);
+        
+        // $show.value = $show.value + String(a); 
+        
+    });
+        
+
+    
 }) ();
